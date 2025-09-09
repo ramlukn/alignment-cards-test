41fc80d26e699ef679a80b043b5af7dac1c2e39c
ramlukn <nikhilr.ramlukan@gmail.com>
2025-09-08 21:51:51 -0400

    Create prototype0.js

---
+++ prototype0.js
@@ -0,0 +1,33 @@
+export const cards = [
+  {
+    "name": "xxx",
+    "definition": "xxx",
+    "failureMode": "xxx",
+    "example": "xxx"
+  },    
+  {
+    "name": "xxx",
+    "definition": "xxx",
+    "failureMode": "xxx",
+    "example": "xxx"
+  },
+  {
+    "name": "xxx",
+    "definition": "xxx",
+    "failureMode": "xxx",
+    "example": "xxx"
+  },
+  {
+    "name": "xxx",
+    "definition": "xxx",
+    "failureMode": "xxx",
+    "example": "xxx"
+  },
+  {
+    "name": "xxx",
+    "definition": "xxx",
+    "failureMode": "xxx",
+    "example": "xxx"
+  },
+];
+
